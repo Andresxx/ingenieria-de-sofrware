698ebf0b9c6651049157b3058e1b9544b4dcb7c6
gsaavedra <gsaavedra@aitbol.com>
2019-12-11 17:38:17 -0300

    added styles and comunication

--- app.rb
+++ app.rb
@@ -2,11 +2,14 @@ require 'sinatra'
 
 class App < Sinatra::Base
 
+    $number_of_players = 1
+
     get '/' do
         erb :home
     end
 
     get '/form' do
+        @number_of_players = $number_of_players
         erb :form
     end
 
@@ -16,5 +19,9 @@ class App < Sinatra::Base
         @FinalPositionY = params[:PositionY]
         erb :game
     end
-    
+
+    post '/numberOfPlayers' do
+        $number_of_players = params[:players]
+        redirect "/form"
+    end
 end
